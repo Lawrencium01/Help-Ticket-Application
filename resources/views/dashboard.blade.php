5d11ff341ed4017e74d317fd7e817b3dff33eefb
Lawrencium01 <148592891+Lawrencium01@users.noreply.github.com>
2024-07-26 00:58:50 +0100

    first commit before learning migration

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,31 @@
+<x-app-layout>
+   <div class="row flex">
+        <div class="col-3" style="padding: 60px;">
+            <img src="/JPG/freecodecamp.jpg" style="border-radius:50%">
+        </div>
+        <div class="col-9" style="padding-top: 60px;">
+            <div>
+                <h1>freecodecamp</h1>
+            </div>
+            <div class="flex">
+                <div style="padding-right:20px"><strong>153</strong>posts</div>
+                <div style="padding-right:20px"><strong>23k</strong>followers</div>
+                <div style="padding-right:20px"><strong>212</strong>following</div>
+            </div>
+            <div style="padding-top:15px; font-size:18px;">freeCodeCamp.org</div>
+            <div>We are a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+   </div>
+   <div class="row flex">
+        <div class="col-4">
+            <img src="https://i.ytimg.com/vi/MYyJ4PuL4pY/0.jpg" style="margin-right: 50px;">
+        </div>
+        <div class="col-4">
+            <img src="https://i.ytimg.com/vi/MYyJ4PuL4pY/0.jpg"  style="margin-right: 50px;">
+        </div>
+        <div class="col-4">
+            <img src="https://i.ytimg.com/vi/MYyJ4PuL4pY/0.jpg" style="margin-right: 50px;">
+        </div>      
+   </div>
+</x-app-layout>
