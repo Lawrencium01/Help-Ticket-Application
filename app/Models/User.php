c535f9a628bdb29e5b66e5bfbaaee1a9dece5b0d
Lawrencium01 <148592891+Lawrencium01@users.noreply.github.com>
2024-08-01 22:56:23 +0100

    worked on form request

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Attribute;
+use Illuminate\Support\Str;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -18,6 +21,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'avatar',
         'email',
         'password',
     ];
